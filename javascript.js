60353ee6e1c0c0e1e6c86bd0d45b9f1f953f4b6b
aragalucas <lucasaragao0192@gmail.com>
2023-09-19 13:19:54 -0300

    second part 'but now it is working'

--- javascript.js
+++ javascript.js
@@ -7,21 +7,33 @@ function playRound(playerSelection, computerSelection) {
 let s = "scissor"; 
 let r = "rock";
 let p = "paper";
-if (playerSelection === r && computerSelection === s) {
-    return "player win 'rock beats scissor'"    
-} else if (playerSelection === s && computerSelection === p) {
-    return "player win 'scissor beats paper'"    
-} else if (playerSelection === p && computerSelection === r) {
-    return "player win 'paper beats rock'"    
-} else if (computerSelection === r && playerSelection === s) {
-    return  "computer win 'rock beats scissor'"
-} else if (computerSelection === s && playerSelection === p) {
-    return  "computer win 'scissor beats paper'"
-} else if (computerSelection === p && playerSelection === r) {
-    return  "computer win 'paper beats rock'"
+if (playerSelection == r && computerSelection == s) {
+    return "player win 'rock beats scissor'"
+} 
+else if (playerSelection == s && computerSelection == p) {
+    return "player win 'scissor beats paper'"
+       
+} 
+else if (playerSelection == p && computerSelection == r) {
+    return "player win 'paper beats rock'"
+    
+} 
+else if (computerSelection == r && playerSelection == s) {
+    return "computer win 'rock beats scissor'" 
 }
-
+else if (computerSelection == s && playerSelection == p) {
+    return "computer win 'scissor beats paper'" 
+} 
+else if (computerSelection == p && playerSelection == r) {
+    return "computer win 'paper beats rock'"
+} 
+else if (playerSelection == computerSelection) {
+    return "Tie"
+    
+}
 }
 
 let playerSelection = prompt("what is your choice").toLowerCase();
 let computerSelection = getComputerChoice();
+console.log(playerSelection, computerSelection)
+console.log(playRound(playerSelection, computerSelection))
