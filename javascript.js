e8ec4b13f0efebc69b1db9ae30ad673b43d978f9
aragalucas <lucasaragao0192@gmail.com>
2023-09-19 12:17:59 -0300

    finish second part

--- javascript.js
+++ javascript.js
@@ -3,4 +3,25 @@ let options = ["paper", "rock", "scissor"];
 let choice = options[Math.floor(Math.random()*options.length)];
 return choice
 }
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+let s = "scissor"; 
+let r = "rock";
+let p = "paper";
+if (playerSelection === r && computerSelection === s) {
+    return "player win 'rock beats scissor'"    
+} else if (playerSelection === s && computerSelection === p) {
+    return "player win 'scissor beats paper'"    
+} else if (playerSelection === p && computerSelection === r) {
+    return "player win 'paper beats rock'"    
+} else if (computerSelection === r && playerSelection === s) {
+    return  "computer win 'rock beats scissor'"
+} else if (computerSelection === s && playerSelection === p) {
+    return  "computer win 'scissor beats paper'"
+} else if (computerSelection === p && playerSelection === r) {
+    return  "computer win 'paper beats rock'"
+}
+
+}
+
+let playerSelection = prompt("what is your choice").toLowerCase();
+let computerSelection = getComputerChoice();
